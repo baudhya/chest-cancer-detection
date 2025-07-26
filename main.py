b02ec0f0331a3531e7d3f2cfcc9a63e2c4395b48
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2025-07-26 13:53:33 +0530

    change function into class for argument parsing

--- main.py
+++ main.py
@@ -17,49 +17,50 @@ from utils.plot import (
 )
 
 from utils.config import Configuration
-from utils.argument_parser import get_args
+from utils.argument_parser import Argument
 
 #Hyper parameter's 
 IMG_SIZE = 200 # original_image -> 200 x 200 size . Use 224 x 224 for best output (currently not fitting inside my GPU)
 
-args = get_args()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-train_transform, val_transform = get_transformation(IMG_SIZE)
-train_dataset, test_dataset, val_dataset = get_dataset_processed(
-    'dataset/train', 'dataset/test', 'dataset/valid',
-    train_transform, train_transform, val_transform
-)
-train_loader, test_loader, val_loader = get_dataloader(train_dataset, test_dataset, val_dataset)
 
+if __name__ == "__main__":
+    args = Argument.get_arguments()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_transform, val_transform = get_transformation(IMG_SIZE)
+    train_dataset, test_dataset, val_dataset = get_dataset_processed(
+        'dataset/train', 'dataset/test', 'dataset/valid',
+        train_transform, train_transform, val_transform
+    )
+    train_loader, test_loader, val_loader = get_dataloader(train_dataset, test_dataset, val_dataset)
 
-class_names = train_dataset.classes
-print("Classes:", class_names)
+    class_names = train_dataset.classes
+    model = get_model(len(class_names), args.model_name, True).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr) 
+    config = Configuration(model, criterion, optimizer, train_loader, val_loader, args.num_epoch, args.lr, device)
+    
+    print("Model : ", model)
+    print("Classes:", class_names)
 
-model = get_model(len(class_names), args.model_name, True).to(device)
-print("Model : ", model)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    trainer = Trainer(config)
+    trainer.train()
+    test_acc, test_loss = trainer.evaluate(test_loader)
 
-config = Configuration(model, criterion, optimizer, train_loader, val_loader, args.num_epoch, args.lr, device)
-trainer = Trainer(config)
-trainer.train()
-test_acc, test_loss = trainer.evaluate(test_loader)
-print(f"Final Test Accuracy: {test_acc:.2f}%")
+    print(f"Final Test Accuracy: {test_acc:.2f}%")
+    plot_epoch_graph(
+        args.num_epoch, 
+        trainer.get_train_loss_list(), "Train Loss",
+        trainer.get_val_loss_list(), "Validation Loss", 
+        "Loss"
+    )
 
-plot_epoch_graph(
-    args.num_epoch, 
-    trainer.get_train_loss_list(), "Train Loss",
-    trainer.get_val_loss_list(), "Validation Loss", 
-    "Loss"
-)
+    plot_epoch_graph(
+        args.num_epoch,
+        trainer.get_train_acc_list(), "Train Accuracy",
+        trainer.get_val_acc_list(), "Validation Accuracy",
+        "Accuracy"
+    )
 
-plot_epoch_graph(
-    args.num_epoch,
-    trainer.get_train_acc_list(), "Train Accuracy",
-    trainer.get_val_acc_list(), "Validation Accuracy",
-    "Accuracy"
-)
-
-y_true, y_pred = get_classification_data(model, test_loader, device)
-print_classification_report(y_true, y_pred, class_names)
-plot_confusion_matrix(y_true, y_pred, class_names)
+    y_true, y_pred = get_classification_data(model, test_loader, device)
+    print_classification_report(y_true, y_pred, class_names)
+    plot_confusion_matrix(y_true, y_pred, class_names)
--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -1,11 +1,10 @@
 import argparse
 
-def get_args():
-    parser = argparse.ArgumentParser(description="Training Configuration")
-
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
-    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
-    parser.add_argument('--num_epoch', type=int, default=10, help='Number of epochs')
-    parser.add_argument('--model_name', type=str, default='densenet', help='Model architecture name')
-
-    return parser.parse_args()
+class Argument():
+    def get_arguments():
+        parser = argparse.ArgumentParser(description="Training Configuration")
+        parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+        parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+        parser.add_argument('--num_epoch', type=int, default=10, help='Number of epochs')
+        parser.add_argument('--model_name', type=str, default='densenet', help='Model architecture name')
+        return parser.parse_args()
