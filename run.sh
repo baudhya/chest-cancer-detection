7ebfb5e585a9126fcc566f97a59bbf67f9234334
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2025-08-12 13:17:55 +0530

    Add run.sh for running all models sequential

---
+++ run.sh
@@ -0,0 +1,4 @@
+python main.py --batch_size 32 --lr 0.0001 --num_epoch 100 --model_name custom
+python main.py --batch_size 32 --lr 0.0001 --num_epoch 50 --model_name resnet
+python main.py --batch_size 32 --lr 0.0001 --num_epoch 50 --model_name densenet
+python main.py --batch_size 32 --lr 0.0001 --num_epoch 50 --model_name vgg
